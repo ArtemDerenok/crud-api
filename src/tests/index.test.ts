fcd5a880c36f0181df0373d4731f7b22c7994629
ArtemDerenok <derenokartem95@gmail.com>
2023-04-26 21:39:43 +0300

    feat: add tests

---
+++ src/tests/index.test.ts
@@ -0,0 +1,50 @@
+import * as request from 'supertest';
+import server from '../server';
+import { IUserBody } from '../utils/interfaces';
+
+const req = request(server);
+
+describe('GET api/users', () => {
+  it('It should return all users', async () => {
+    const res = await req.get('/api/users');
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+});
+
+describe('POST api/users', () => {
+  it('It should post new user', async () => {
+    const data: IUserBody = {
+      username: 'Test',
+      age: 30,
+      hobbies: [],
+    };
+
+    const res = await req.post('/api/users').send(JSON.stringify(data));
+    expect(JSON.parse(res.text)).toBe('User was created');
+  });
+});
+
+describe('GET user by id api/users/{userID}', () => {
+  it('It should get user', async () => {
+    const resTestUser = await req.get('/api/users');
+    const { id } = resTestUser.body[0];
+
+    const res = await req.get(`/api/users/?id=${id}`);
+    expect(res.body.id).toBe(id);
+  });
+});
+
+describe('PUT user by id api/users/{userID}', () => {
+  it('It should update user', async () => {
+    const resTestUser = await req.get('/api/users');
+    const { id } = resTestUser.body[0];
+    const data: IUserBody = {
+      username: 'Test',
+      age: 30,
+      hobbies: [],
+    };
+
+    const res = await req.put(`/api/users/?id=${id}`).send(JSON.stringify(data));
+    expect(JSON.parse(res.text)).toBe('User was updated');
+  });
+});
